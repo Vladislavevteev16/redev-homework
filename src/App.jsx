f0de8b9c8db74f43a799510bd551d804c4b38e10
Vladislav <Vbabangida@bk.ru>
2026-01-16 09:56:27 +0300

    refactor: clean project tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-import { CounterButton } from "./components/CounterButton";
-import { SearchInput } from "./components/SearchInput";
-import { ItemList } from "./components/ItemList";
-import { createUserList } from "./utils/createUserList";
-import { useState, useCallback } from "react";
+
 import "./App.css";
 
 function App() {
-  const [userList] = useState(() => createUserList());
-  const [searchQuery, setSearchQuery] = useState("");
-  const [count, setCount] = useState(0);
-
-  const handleQueryChange = useCallback(({ target: { value } }) => {
-    setSearchQuery(value);
-  }, []);
-
-  const handleCountIncrement = useCallback(
-    () => setCount((prev) => prev + 1),
-    []
-  );
+ 
 
   return (
     <div className="main-container">
-      <SearchInput
-        handleQueryChange={handleQueryChange}
-        searchQuery={searchQuery}
-      />
-      <ItemList searchQuery={searchQuery} userList={userList} />
-      <CounterButton onClick={handleCountIncrement} count={count} />
+     
     </div>
   );
 }
